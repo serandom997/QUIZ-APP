f86543c358f7071cc5bc7355cc5481417d077843
Shaoen <91254558+serandom997@users.noreply.github.com>
2022-02-28 16:10:38 +0800

    Add files via upload

---
+++ main/code/newfile.java
@@ -0,0 +1,15 @@
+package com.example.csit_assignment_2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class newfile extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newfie);
+
+
+    }
+}
